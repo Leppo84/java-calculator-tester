5aae32f07ce8066d27ba2696ef20c2975d4b3ab6
LeppoXena <francesco.lepore@xenagames.com>
2023-02-09 17:31:09 +0100

    Exception on division tested

--- src/main/java/calculatorMaven/Calculator.java
+++ src/main/java/calculatorMaven/Calculator.java
@@ -17,9 +17,13 @@ public class Calculator {
 		return num1 * num2;	
 	}
 
-	public float division( float num1, float num2) {
-		
-		return num1 / num2;	
+	public float division( float num1, float num2) throws Exception{
+		if (num2 != 0) {
+		return num1 / num2;
+		}
+		else {
+			throw new Exception("La divisione per 0 dà infinito");
+		}
 	}
 
 }
--- src/test/java/calculatorMaven/CalculatorTest.java
+++ src/test/java/calculatorMaven/CalculatorTest.java
@@ -1,6 +1,7 @@
 package calculatorMaven;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 //import calculatorMaven.Calculator;
@@ -27,9 +28,28 @@ class CalculatorTest {
 	}
 
 	@Test
-	void testDivision() {
+	void testDivision() throws Exception{
 		float result = calc.division(2,4);
 		assertEquals(0.5, result, "La divisione deve essere 0,5");
 	}
 
+	@Test
+	void testDivision2() throws Exception{
+		float result = calc.division(0,4);
+		assertEquals(0, result, "La divisione deve essere 0");
+	}
+
+	@Test
+	void testDivision3() throws Exception{
+		try {
+			calc.division(4,0);			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		assertThrows(
+				Exception.class, 
+				() -> calc.division(4,0),
+				"deve essere generata un'eccezione");
+	}
+	
 }

ba3b1fea02d3c8644b83d8191ddd57aadf987ede
LeppoXena <francesco.lepore@xenagames.com>
2023-02-09 16:55:15 +0100

    Fix last commit

--- src/main/java/calculatorMaven/Calculator.java
+++ src/main/java/calculatorMaven/Calculator.java
@@ -10,15 +10,16 @@ public class Calculator {
 	}
 
 	public float subtraction( float num1, float num2) {
-		return num1 + num2;	
+		return num1 - num2;	
 	}
 
 	public float multiplication( float num1, float num2) {
-		return num1 + num2;	
+		return num1 * num2;	
 	}
 
 	public float division( float num1, float num2) {
-		return num1 + num2;	
+		
+		return num1 / num2;	
 	}
 
 }
